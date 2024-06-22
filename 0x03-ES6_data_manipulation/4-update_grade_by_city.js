801b9044549e46b0cdf5cdde77091535c1d476e0
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-06-22 06:28:00 +0000

    manipulare data

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
-export default function updateStudentGradeByCity(students_list, city, newGrades) {
-  return student_list.filter(student => student.location === city)
-	.map((student) => {
-	  const grades = newGrades.find(grade => grade.student_id === student.id)
-	  return {...student, grade: grades ? grades.grade : 'N/A'}
-	}
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  return studentList.filter((student) => student.location === city)
+    .map((student) => {
+      const grades = newGrades.find((grade) => grade.student_id === student.id);
+      return { ...student, grade: grades ? grades.grade : 'N/A' };
+    });
 }
