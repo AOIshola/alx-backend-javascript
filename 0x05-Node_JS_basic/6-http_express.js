8744bfbaba2d88d9e293049e833f626885c29952
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-08-21 13:03:29 +0000

    fix lint errors

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,14 +1,15 @@
 const express = require('express');
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 const PORT = 1245;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
 
 module.exports = app;
