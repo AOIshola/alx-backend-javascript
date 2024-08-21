35dcda70964244fc56d134616301f7a24c6887ed
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-08-21 11:53:13 +0000

    fix eol errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,11 +2,11 @@ process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on('data', (data) => {
     const name = data.toString().trim()
-    process.stdout.write(`Your name is: ${name}\n`)
+    process.stdout.write(`Your name is: ${name}\r`)
 
     process.exit()
 });
 
 process.on('exit', async () => {
-    process.stdout.write("This important software is now closing\n");
+    process.stdout.write("This important software is now closing\r");
 });
