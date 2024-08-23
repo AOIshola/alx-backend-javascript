19182fbb56aa3980fe3a0dafb34bf617e99e1557
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-08-23 10:50:06 +0000

    fix lint errors

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -5,18 +5,19 @@ const { promises: fsp } = fs;
 export async function readDatabase(filePath) {
   try {
     const data = await fsp.readFile(filePath, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
 
     if (lines.length <= 1) {
       throw new Error('Cannot load the database');
     }
 
     const headers = lines[0].split(',');
-    const students = lines.slice(1).map(line => line.split(',').map(value => value.trim())).filter(student => student.length === headers.length);
+    const students = lines.slice(1).map((line) => line.split(',').map((value) => value.trim()))
+    .filter((student) => student.length === headers.length);
 
     const fieldCounts = {};
 
-    students.forEach(student => {
+    students.forEach((student) => {
       const field = student[3];
       const firstName = student[0];
 
